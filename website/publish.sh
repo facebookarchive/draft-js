bea4d01bd8759be2151736b2f4f0973f3af1115b
Ben Alpert <balpert@fb.com>
2016-02-24 13:50:08 -0800

    On Travis, fail build if any page fails (like RN)

--- website/publish.sh
+++ website/publish.sh
@@ -17,7 +17,10 @@ node server/generate.js
 cp -R build/draft-js/* ../../draft-js-gh-pages/
 rm -Rf build/
 cd ../../draft-js-gh-pages
-git add --all
-git commit -m "update website"
-git push
+git status
+git add -A .
+if ! git diff-index --quiet HEAD --; then
+  git commit -m "update website"
+  git push origin gh-pages
+fi
 cd ../draft-js/website
--- website/server/generate.js
+++ website/server/generate.js
@@ -1,63 +1,66 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
+var Promise = require('bluebird');
 var request = require('request');
 var glob = require('glob');
 var fs = require('fs.extra');
 var mkdirp = require('mkdirp');
 var server = require('./server.js');
 
+require('./convert.js')();
+server.noconvert = true;
+
 // Sadly, our setup fatals when doing multiple concurrent requests
 // I don't have the time to dig into why, it's easier to just serialize
 // requests.
-var queue = (function() {
-  var is_executing = false;
-  var queue = [];
-  function push(fn) {
-    queue.push(fn);
-    execute();
-  }
-  function execute() {
-    if (is_executing) {
-      return;
-    }
-    if (queue.length === 0) {
-      return;
-    }
-    var fn = queue.shift();
-    is_executing = true;
-    fn(function() {
-      is_executing = false;
-      execute()
-    });
-  }
-  return {push: push};
-})();
+var queue = Promise.resolve();
 
 glob('src/**/*.*', function(er, files) {
-  var count = files.length;
-
   files.forEach(function(file) {
     var targetFile = file.replace(/^src/, 'build');
 
     if (file.match(/\.js$/) && !file.match(/\/lib\//)) {
       targetFile = targetFile.replace(/\.js$/, '.html');
-      queue.push(function(cb) {
-        request('http://localhost:8079/' + targetFile.replace(/^build\//, ''), function(error, response, body) {
-          mkdirp.sync(targetFile.replace(new RegExp('/[^/]*$'), ''));
-          fs.writeFileSync(targetFile, body);
-          cb();
+      queue = queue.then(function() {
+        return new Promise(function(resolve, reject) {
+          request('http://localhost:8079/' + targetFile.replace(/^build\//, ''), function(error, response, body) {
+            if (error) {
+              reject(error);
+              return;
+            }
+            if (response.statusCode != 200) {
+              reject(new Error('Status ' + response.statusCode + ':\n' + body));
+              return;
+            }
+            mkdirp.sync(targetFile.replace(new RegExp('/[^/]*$'), ''));
+            fs.writeFileSync(targetFile, body);
+            resolve();
+          });
         });
       });
     } else {
-      queue.push(function(cb) {
-        mkdirp.sync(targetFile.replace(new RegExp('/[^/]*$'), ''));
-        fs.copy(file, targetFile, cb);
+      queue = queue.then(function() {
+        return new Promise(function(resolve, reject) {
+          mkdirp.sync(targetFile.replace(new RegExp('/[^/]*$'), ''));
+          fs.copy(file, targetFile, resolve);
+        });
       });
     }
   });
 
-  queue.push(function(cb) {
-    server.close();
+  queue = queue.then(function() {
     console.log('It is live at: http://facebook.github.io/draft-js/')
-    cb();
+  }).finally(function() {
+    server.close();
+  }).catch(function(e) {
+    console.error(e);
+    process.exit(1);
   });
 });
