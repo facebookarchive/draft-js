69cfc608095ab33b18b04399ad58a2cef50b2fa3
Alex Z <zelenskiy.alex@gmail.com>
2016-03-07 16:15:18 -0800

    Expose html to block converter through Draft object

--- src/Draft.js
+++ src/Draft.js
@@ -26,6 +26,8 @@ const RichTextEditorUtil = require('RichTextEditorUtil');
 const SelectionState = require('SelectionState');
 
 const convertFromDraftStateToRaw = require('convertFromDraftStateToRaw');
+const convertFromHTMLToContentBlocks = 
+  require('convertFromHTMLToContentBlocks');
 const convertFromRawToDraftState = require('convertFromRawToDraftState');
 const generateRandomKey = require('generateRandomKey');
 const getDefaultKeyBinding = require('getDefaultKeyBinding');
@@ -48,6 +50,7 @@ const DraftPublic = {
   Modifier: DraftModifier,
   RichUtils: RichTextEditorUtil,
 
+  convertFromHTMLToContentBlocks,
   convertFromRaw: convertFromRawToDraftState,
   convertToRaw: convertFromDraftStateToRaw,
   genKey: generateRandomKey,
