36a5db3fbdbe89c51b51b0ab2a96023ee4cc520b
jaaberg <aaberg89@gmail.com>
2016-04-09 22:27:01 +0200

    Remove h3-h6 from the lightweight editor demo

--- website/src/draft-js/index.js
+++ website/src/draft-js/index.js
@@ -147,10 +147,6 @@ class StyleButton extends React.Component {
 const BLOCK_TYPES = [
   {label: 'H1', style: 'header-one'},
   {label: 'H2', style: 'header-two'},
-  {label: 'H3', style: 'header-three'},
-  {label: 'H4', style: 'header-four'},
-  {label: 'H5', style: 'header-five'},
-  {label: 'H6', style: 'header-six'},
   {label: 'Blockquote', style: 'blockquote'},
   {label: 'UL', style: 'unordered-list-item'},
   {label: 'OL', style: 'ordered-list-item'},
