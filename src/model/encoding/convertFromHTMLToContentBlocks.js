9be52a33ac0935d54b8068a10d81a030f630d1ad
Marcel Cutts <marcel.cutts@red-badger.com>
2016-05-05 17:14:37 +0100

    Fixed extremely minor typo, hare -> here

--- src/model/encoding/convertFromHTMLToContentBlocks.js
+++ src/model/encoding/convertFromHTMLToContentBlocks.js
@@ -426,7 +426,7 @@ function convertFromHTMLtoContentBlocks(
   html: string,
   DOMBuilder: Function = getSafeBodyFromHTML,
 ): ?Array<ContentBlock> {
-  // Be ABSOLUTELY SURE that the dom builder you pass hare won't execute
+  // Be ABSOLUTELY SURE that the dom builder you pass here won't execute
   // arbitrary code in whatever environment you're running this in. For an
   // example of how we try to do this in-browser, see getSafeBodyFromHTML.
 
