dec48e01caece45c72a9687d9435e0f522fff259
Eric Coan <ecoan@instructure.com>
2016-06-17 13:09:29 -0600

    Improve Regex

--- src/model/encoding/convertFromHTMLToContentBlocks.js
+++ src/model/encoding/convertFromHTMLToContentBlocks.js
@@ -45,8 +45,8 @@ var MAX_DEPTH = 4;
 var REGEX_CR = new RegExp('\r', 'g');
 var REGEX_LF = new RegExp('\n', 'g');
 var REGEX_NBSP = new RegExp(NBSP, 'g');
-var REGEX_CARRIAGE = new RegExp('(&#13;|&#13)', 'g');
-var REGEX_ZWS = new RegExp('(&#8203;|&#8203)', 'g');
+var REGEX_CARRIAGE = new RegExp('&#13;?', 'g');
+var REGEX_ZWS = new RegExp('&#8203;?', 'g');
 
 // Block tag flow is different because LIs do not have
 // a deterministic style ;_;
