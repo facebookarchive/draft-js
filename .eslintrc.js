873a2deb999dd415f1d9f03df0f05b3c8ec63040
Paul O’Shannessy <paul@oshannessy.com>
2016-07-12 14:12:18 -0700

    Use eslint-config-fbjs for lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,17 +8,5 @@
  */
 
 module.exports = {
-  parser: 'babel-eslint',
-
-  extends: './node_modules/fbjs-scripts/eslint/.eslintrc.js',
-
-  plugins: [
-    'react',
-  ],
-
-  rules: {
-    'react/jsx-uses-react': 1,
-    'react/jsx-uses-vars': 1,
-    'react/react-in-jsx-scope': 1,
-  },
+  extends: 'fbjs',
 };
