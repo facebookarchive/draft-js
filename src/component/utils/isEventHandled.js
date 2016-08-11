c14774f98ed5fa0a5fcd3c65322d54dbd08d95f9
Johannes Lumpe <johannes@johanneslumpe.de>
2016-08-11 11:53:07 +0300

    handle `true` value for backwards compatibility

--- src/component/utils/isEventHandled.js
+++ src/component/utils/isEventHandled.js
@@ -20,7 +20,7 @@ import type {DraftHandleValue} from 'DraftHandleValue';
  * from a handler indicates that it was handled.
  */
 function isEventHandled(value: DraftHandleValue): boolean {
-  return value === 'handled';
+  return value === 'handled' || value === true;
 }
 
 module.exports = isEventHandled;
