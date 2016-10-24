6626cae5f1b04a43db29d380691ae1e29dd68eb7
Tyler Craft <tyler@tylercraft.com>
2016-10-24 12:01:31 -0700

    Chrome fix

--- src/model/encoding/convertFromHTMLToContentBlocks.js
+++ src/model/encoding/convertFromHTMLToContentBlocks.js
@@ -368,7 +368,37 @@ function genFragment(
     ) {
       return {chunk: getBlockDividerChunk('unstyled', depth), entityMap};
     }
-    return {chunk:getSoftNewlineChunk(), entityMap};
+    return {chunk: getSoftNewlineChunk(), entityMap};
+  }
+
+  // IMG tags
+  if (
+    nodeName === 'img' &&
+    node instanceof HTMLImageElement &&
+    node.attributes.getNamedItem('src') &&
+    node.attributes.getNamedItem('src').value
+  ) {
+    const image: HTMLImageElement = node;
+    const entityConfig = {};
+
+    imgAttr.forEach((attr) => {
+      const imageAttribute = image.getAttribute(attr);
+      if (imageAttribute) {
+        entityConfig[attr] = imageAttribute;
+      }
+    });
+    const imageURI = new URI(entityConfig.src).toString();
+    node.textContent = imageURI; // Output src if no decorator
+
+    newEntityMap = addEntityToEntityMap(
+      newEntityMap,
+      new DraftEntityInstance({
+        type: 'IMAGE',
+        mutability: 'MUTABLE',
+        data: entityConfig || {},
+      })
+    );
+    inEntity = newEntityMap.keySeq().last();
   }
 
   var chunk = getEmptyChunk();
@@ -434,33 +464,6 @@ function genFragment(
         newEntityMap,
         new DraftEntityInstance({
           type: 'LINK',
-          mutability: 'MUTABLE',
-          data: entityConfig || {},
-        })
-      );
-      entityId = newEntityMap.keySeq().last();
-    } else if (
-      child instanceof HTMLImageElement &&
-      child.src
-    ) {
-      const image: HTMLImageElement = child;
-      const entityConfig = {};
-
-      imgAttr.forEach((attr) => {
-        const imageAttribute = image.getAttribute(attr);
-        if (imageAttribute) {
-          entityConfig[attr] = imageAttribute;
-        }
-      });
-
-      const imageURI = new URI(image.src).toString();
-      entityConfig.url = imageURI;
-      child.textContent = imageURI; // Output src if no decorator
-
-      newEntityMap = addEntityToEntityMap(
-        newEntityMap,
-        new DraftEntityInstance({
-          type: 'IMAGE',
           mutability: 'MUTABLE',
           data: entityConfig || {},
         })
