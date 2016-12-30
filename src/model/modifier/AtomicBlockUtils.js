b7fadfffec410b3332e056767f627324ecc5b725
delijah <raffael.wannenmacher@gmail.com>
2016-12-30 01:08:19 +0100

    Moving atomic blocks (#551)
    * Implemented moveAtomicBlockBefore and moveAtomicBlockAfter in AtomicBlockUtils
    
    * Renamed EditorChangeType 'change-fragment' to 'move-block' and added to docs
    
    * Changed interface for moving atomic blocks:
    
    - Just use one function called moveAtomicBlock instead of three
    - Introduce DraftInsertionType for determining if a fragment shall replace another fragment, or if it shall be inserted before or after another fragment
    
    * Using ES6 variable declaration and getStart/EndKey instead of getAnchor/FocusKey

--- src/model/modifier/AtomicBlockUtils.js
+++ src/model/modifier/AtomicBlockUtils.js
@@ -18,9 +18,13 @@ const CharacterMetadata = require('CharacterMetadata');
 const ContentBlock = require('ContentBlock');
 const DraftModifier = require('DraftModifier');
 const EditorState = require('EditorState');
+const SelectionState = require('SelectionState');
 const Immutable = require('immutable');
 
 const generateRandomKey = require('generateRandomKey');
+const moveBlockInContentState = require('moveBlockInContentState');
+
+import type {DraftInsertionType} from 'DraftInsertionType';
 
 const {
   List,
@@ -83,6 +87,84 @@ const AtomicBlockUtils = {
     });
 
     return EditorState.push(editorState, newContent, 'insert-fragment');
+  },
+
+  moveAtomicBlock: function(
+    editorState: EditorState,
+    atomicBlock: ContentBlock,
+    targetRange: SelectionState,
+    insertionMode?: DraftInsertionType
+  ): EditorState {
+    const contentState = editorState.getCurrentContent();
+    const selectionState = editorState.getSelection();
+
+    let withMovedAtomicBlock;
+
+    if (insertionMode === 'before' || insertionMode === 'after') {
+      const targetBlock = contentState.getBlockForKey(
+        insertionMode === 'before' ?
+          targetRange.getStartKey() :
+          targetRange.getEndKey()
+      );
+
+      withMovedAtomicBlock = moveBlockInContentState(
+        contentState,
+        atomicBlock,
+        targetBlock,
+        insertionMode
+      );
+    } else {
+      const afterRemoval = DraftModifier.removeRange(
+        contentState,
+        targetRange,
+        'backward'
+      );
+
+      const selectionAfterRemoval = afterRemoval.getSelectionAfter();
+      const targetBlock = afterRemoval.getBlockForKey(
+        selectionAfterRemoval.getFocusKey()
+      );
+
+      if (selectionAfterRemoval.getStartOffset() === 0) {
+        withMovedAtomicBlock = moveBlockInContentState(
+          afterRemoval,
+          atomicBlock,
+          targetBlock,
+          'before'
+        );
+      } else if (selectionAfterRemoval.getEndOffset() === targetBlock.getLength()) {
+        withMovedAtomicBlock = moveBlockInContentState(
+          afterRemoval,
+          atomicBlock,
+          targetBlock,
+          'after'
+        );
+      } else {
+        const afterSplit = DraftModifier.splitBlock(
+          afterRemoval,
+          selectionAfterRemoval
+        );
+
+        const selectionAfterSplit = afterSplit.getSelectionAfter();
+        const targetBlock = afterSplit.getBlockForKey(
+          selectionAfterSplit.getFocusKey()
+        );
+
+        withMovedAtomicBlock = moveBlockInContentState(
+          afterSplit,
+          atomicBlock,
+          targetBlock,
+          'before'
+        );
+      }
+    }
+
+    const newContent = withMovedAtomicBlock.merge({
+      selectionBefore: selectionState,
+      selectionAfter: withMovedAtomicBlock.getSelectionAfter().set('hasFocus', true),
+    });
+
+    return EditorState.push(editorState, newContent, 'move-block');
   },
 };
 
