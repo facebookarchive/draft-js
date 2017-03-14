ac150b77af90c512d610155330354d8592ec466b
Flarnie Marchan <flarnie@fb.com>
2017-03-14 16:30:45 -0700

    Update universal example
    since this PR was created some changes were made and this updates the
    example to fit with our other examples.
    
    - moves into the version specific subdirectories
    - updates package.json
    - removes some ES6 destucturing temporarily because polyfill seems to be
      not loading correctly

--- examples/draft-0-10-0/universal/editor.js
+++ examples/draft-0-10-0/universal/editor.js
@@ -1,3 +1,16 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule Draft
+ */
+
+'use strict';
+
 const Draft = require('draft-js');
 const React = require('react');
 
@@ -9,8 +22,8 @@ class SimpleEditor extends React.Component {
     this.onChange = (editorState) => this.setState({editorState});
   }
   render() {
-    const {Editor} = Draft;
-    const {editorState} = this.state;
+    const Editor = Draft.Editor;
+    const editorState = this.state.editorState;
     return <Editor placeholder="heyyyyy" editorKey="foobaz" editorState={editorState} onChange={this.onChange} />;
   }
 }
--- examples/universal/client.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var ReactDom = require('react-dom');
-
-var SimpleEditor = require('./editor.js').SimpleEditor;
-
-ReactDom.render(<SimpleEditor />, document.getElementById('react-content'));
