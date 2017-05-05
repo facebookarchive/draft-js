4760eed04613e05f6ecc92edcc6b51cbe65cb1a8
Holsopple, Dirk <dholsopple@cvent.com>
2017-05-05 12:36:25 -0400

    Add check for selection type of None

--- src/component/selection/setDraftEditorSelection.js
+++ src/component/selection/setDraftEditorSelection.js
@@ -234,7 +234,9 @@ function addFocusToSelection(
         selectionState: JSON.stringify(selectionState.toJS()),
       });
     }
-    selection.extend(node, offset);
+    if (selection.type !== 'None') {
+      selection.extend(node, offset);
+    }
   } else {
     // IE doesn't support extend. This will mean no backward selection.
     // Extract the existing selection range and add focus to it.
