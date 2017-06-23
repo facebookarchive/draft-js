8622f4bea06b0743780e6d22967e8266f0b44ee8
Fyodor Ananiev <tedmx@ya.ru>
2017-06-23 18:38:12 +0300

    Add tests

--- src/model/transaction/__tests__/insertFragmentIntoContentState-test.js
+++ src/model/transaction/__tests__/insertFragmentIntoContentState-test.js
@@ -32,6 +32,7 @@ describe('insertFragmentIntoContentState', () => {
   var block = content.getBlockMap().first();
   var data = new Map({a: 1});
   var secondData = new Map({b: 2});
+  var thirdData = new Map({c: 2});
 
   function createFragment() {
     var fragmentArray = [
@@ -61,6 +62,53 @@ describe('insertFragmentIntoContentState', () => {
         text: 'yy',
         characterList: List.of(EMPTY, EMPTY),
         data: secondData,
+      }),
+    ];
+    return BlockMapBuilder.createFromArray(fragmentArray);
+  }
+
+  function createFragmentWithAtomicStart() {
+    var fragmentArray = [
+      new ContentBlock({
+        key: 'j',
+        type: 'atomic',
+        text: ' ',
+        characterList: List.of(EMPTY),
+        data: data,
+      }),
+      new ContentBlock({
+        key: 'k',
+        type: 'unstyled',
+        text: 'yy',
+        characterList: List.of(EMPTY, EMPTY),
+        data: secondData,
+      }),
+    ];
+    return BlockMapBuilder.createFromArray(fragmentArray);
+  }
+
+  function createFragmentWithAtomicBounds() {
+    var fragmentArray = [
+      new ContentBlock({
+        key: 'j',
+        type: 'atomic',
+        text: ' ',
+        characterList: List.of(EMPTY),
+        data: data,
+      }),
+      new ContentBlock({
+        key: 'k',
+        type: 'unstyled',
+        text: 'yy',
+        characterList: List.of(EMPTY, EMPTY),
+        data: secondData,
+      }),
+      new ContentBlock({
+        key: 'i',
+        type: 'atomic',
+        text: ' ',
+        characterList: List.of(EMPTY),
+        data: thirdData,
       }),
     ];
     return BlockMapBuilder.createFromArray(fragmentArray);
@@ -148,6 +196,130 @@ describe('insertFragmentIntoContentState', () => {
     expect(newBlock.getData()).toBe(data);
     expect(secondBlock.getText().slice(0, 2)).toBe('yy');
     expect(secondBlock.getData()).toBe(secondData);
+  });
+
+  it(
+    `must apply multiblock fragments starting with 'atomic'
+    to the start`,
+    () => {
+      var fragment = createFragmentWithAtomicStart();
+      var modified = insertFragmentIntoContentState(
+        content,
+        selection,
+        fragment,
+      );
+
+      var blocks = modified.getBlockMap().toArray();
+
+      expect(blocks.length).toBe(4);
+      expect(blocks[0].getType()).toBe('atomic');
+      expect(blocks[0].getData()).toBe(data);
+      expect(blocks[1].getType()).toBe('unstyled');
+      expect(blocks[1].getText().slice(0, 2)).toBe('yy');
+      expect(blocks[2].getType()).toBe(
+        content.getBlockMap().skip(1).first().getType(),
+      );
+      expect(blocks[2].getText()).toBe(
+        content.getBlockMap().skip(1).first().getText(),
+      );
+    },
+  );
+
+  it(
+    `must apply multiblock fragments starting with 'atomic'
+    at the end`,
+    () => {
+      var length = block.getLength();
+      var target = selection.merge({
+        focusOffset: length,
+        anchorOffset: length,
+        isBackward: false,
+      });
+
+      var fragment = createFragmentWithAtomicStart();
+      var modified = insertFragmentIntoContentState(
+        content,
+        target,
+        fragment,
+      );
+
+      var blocks = modified.getBlockMap().toArray();
+
+      expect(blocks.length).toBe(5);
+
+      expect(blocks[0].getText()).toBe(block.getText());
+      expect(blocks[0].getData()).toBe(block.getData());
+
+      expect(blocks[1].getType()).toBe('atomic');
+      expect(blocks[1].getData()).toBe(data);
+      expect(blocks[2].getType()).toBe('unstyled');
+      expect(blocks[2].getText()).toBe('yy');
+    },
+  );
+
+  it(
+    `must apply multiblock fragments starting with 'atomic'
+    to within block`,
+    () => {
+      var fragment = createFragmentWithAtomicStart();
+      var originalFirstBlock = content.getBlockMap().first();
+      var target = selection.merge({
+        focusOffset: 2,
+        anchorOffset: 2,
+        isBackward: false,
+      });
+      var modified = insertFragmentIntoContentState(
+        content,
+        target,
+        fragment,
+      );
+
+      var blocks = modified.getBlockMap().toArray();
+
+      expect(blocks.length).toBe(5);
+      expect(blocks[0].getType()).toBe('unstyled');
+      expect(blocks[0].getText()).toBe(
+        originalFirstBlock.getText().slice(0, 2),
+      );
+      expect(blocks[1].getType()).toBe('atomic');
+      expect(blocks[1].getData()).toBe(data);
+      expect(blocks[2].getType()).toBe('unstyled');
+      expect(blocks[2].getText().slice(0, 2)).toBe('yy');
+    },
+  );
+
+  it('must apply multiblock fragments with `atomic`s to within block', () => {
+    var fragment = createFragmentWithAtomicBounds();
+    var originalFirstBlock = content.getBlockMap().first();
+    var target = selection.merge({
+      focusOffset: 2,
+      anchorOffset: 2,
+      isBackward: false,
+    });
+    var modified = insertFragmentIntoContentState(
+      content,
+      target,
+      fragment,
+    );
+
+    var blocks = modified.getBlockMap().toArray();
+
+    expect(blocks.length).toBe(7);
+    expect(blocks[0].getType()).toBe('unstyled');
+    expect(blocks[0].getText()).toBe(
+      originalFirstBlock.getText().slice(0, 2),
+    );
+    expect(blocks[1].getType()).toBe('atomic');
+    expect(blocks[1].getData()).toBe(data);
+    expect(blocks[2].getType()).toBe('unstyled');
+    expect(blocks[2].getText()).toBe('yy');
+    expect(blocks[3].getType()).toBe('atomic');
+    expect(blocks[3].getData()).toBe(thirdData);
+
+    expect(blocks[4].getType()).toBe('unstyled');
+    expect(blocks[4].getText()).toBe(
+      originalFirstBlock.getText().slice(2),
+    );
   });
 
 });
