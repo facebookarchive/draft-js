2f230a9d368cc6e5ff34542d1790de22750d6502
Mathieu M-Gosselin <mathieumg@gmail.com>
2017-06-28 09:52:25 -0400

    Added support for `compositionupdate` event.

--- src/component/handlers/composition/DraftEditorCompositionHandler.js
+++ src/component/handlers/composition/DraftEditorCompositionHandler.js
@@ -44,6 +44,7 @@ const RESOLVE_DELAY = 20;
  */
 let resolved = false;
 let stillComposing = false;
+let compositionUpdates = false;
 let beforeInputData = null;
 let compositionEndData = null;
 
@@ -61,6 +62,13 @@ var DraftEditorCompositionHandler = {
    * `compositionend` events that they fire.
    */
   onBeforeInput: function(editor: DraftEditor, e: SyntheticInputEvent<>): void {
+    if (compositionUpdates) {
+      // We only want to use the `beforeinput` event if the `compositionupdate`
+      // one isn't supported. We know that if it is, it fires before
+      // `beforeinput`.
+      return;
+    }
+
     beforeInputData = (beforeInputData || '') + e.data;
   },
 
@@ -70,6 +78,15 @@ var DraftEditorCompositionHandler = {
    */
   onCompositionStart: function(editor: DraftEditor): void {
     stillComposing = true;
+  },
+
+  /**
+   * A `compositionupdate` event has fired. Update the current composition
+   * session.
+   */
+  onCompositionUpdate: function(editor: DraftEditor, e: SyntheticInputEvent): void {
+    compositionUpdates = true;
+    textInputData = e.data;
   },
 
   /**
