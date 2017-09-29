12ff6e3acbb5ab79c6c02879572fa83c999db231
Matt Krick <matt.krick@gmail.com>
2017-09-29 12:46:38 -0700

    Fix #1326 Safari can't focus with draggable parent
    Summary:
    **Summary**
    
    DraftEditors that lived inside draggable parents were useless in Safari. Now they work.
    Solution found here: https://github.com/react-dnd/react-dnd/issues/178#issuecomment-192021227
    
    **Test Plan**
    
    Use the fiddle in #1326 with the updated version.
    For a quick n dirty, just add this to the stylesheet:
    `div[contenteditable="true"] { user-select: text;}`
    Closes https://github.com/facebook/draft-js/pull/1356
    
    Differential Revision: D5901625
    
    fbshipit-source-id: 7d3797286e4040575b9b5503c776bff1e29b67a9

--- src/component/base/DraftEditor.react.js
+++ src/component/base/DraftEditor.react.js
@@ -225,6 +225,9 @@ class DraftEditor extends React.Component<DraftEditorProps, State> {
 
     const contentStyle = {
       outline: 'none',
+      // fix parent-draggable Safari bug. #1326
+      userSelect: 'text',
+      WebkitUserSelect: 'text',
       whiteSpace: 'pre-wrap',
       wordWrap: 'break-word',
     };
