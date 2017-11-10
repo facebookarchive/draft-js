2d70aa4cebba261bb3ac168718beb36de8b54020
mitermayer <mitermayer.reis@gmail.com>
2017-11-10 09:20:21 -0800

    feature-tree/convert-from-raw-to-state-tree - moving back to js assertions instead of snapshots
    - While the snapshots migration PR is not merged yet we will keep
    writting tests as normal assertions

--- src/model/encoding/__tests__/convertFromRawToDraftState-test.js
+++ src/model/encoding/__tests__/convertFromRawToDraftState-test.js
@@ -16,6 +16,18 @@ jest.disableAutomock();
 
 const convertFromRawToDraftState = require('convertFromRawToDraftState');
 
+const assertDraftState = (rawState, expected) => {
+  // https://github.com/facebook/draft-js/pull/1490
+  // TODO: mitermayer - re-enable snapshot testing after PR [1490] is merged
+  // expect(convertFromRawToDraftState(rawState).getBlockMap()).toMatchSnapshot();
+
+  expect(
+    convertFromRawToDraftState(rawState)
+      .getBlockMap()
+      .toJS(),
+  ).toEqual(expected);
+};
+
 test('must map falsey block types to default value of unstyled', () => {
   const rawState = {
     blocks: [
@@ -26,7 +38,83 @@ test('must map falsey block types to default value of unstyled', () => {
     entityMap: {},
   };
 
-  expect(convertFromRawToDraftState(rawState).getBlockMap()).toMatchSnapshot();
+  assertDraftState(rawState, {
+    A: {
+      key: 'A',
+      type: 'unstyled',
+      text: 'AAAA',
+      characterList: [
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+      ],
+      depth: 0,
+      data: {},
+    },
+    B: {
+      key: 'B',
+      type: 'unstyled',
+      text: 'BBBB',
+      characterList: [
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+      ],
+      depth: 0,
+      data: {},
+    },
+    C: {
+      key: 'C',
+      type: 'unstyled',
+      text: 'CCCC',
+      characterList: [
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+      ],
+      depth: 0,
+      data: {},
+    },
+  });
 });
 
 test('must be able to convert from styled blocks and entities mapped raw state', () => {
@@ -91,7 +179,103 @@ test('must be able to convert from styled blocks and entities mapped raw state',
     },
   };
 
-  expect(convertFromRawToDraftState(rawState).getBlockMap()).toMatchSnapshot();
+  assertDraftState(rawState, {
+    a: {
+      key: 'a',
+      type: 'unstyled',
+      text: 'Alpha',
+      characterList: [
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+      ],
+      depth: 0,
+      data: {},
+    },
+    b: {
+      key: 'b',
+      type: 'unordered-list-item',
+      text: 'Bravo',
+      characterList: [
+        {
+          style: ['BOLD'],
+          entity: '1',
+        },
+        {
+          style: ['BOLD'],
+          entity: '1',
+        },
+        {
+          style: ['BOLD'],
+          entity: '1',
+        },
+        {
+          style: ['BOLD'],
+          entity: '1',
+        },
+        {
+          style: ['BOLD'],
+          entity: '1',
+        },
+      ],
+      depth: 0,
+      data: {},
+    },
+    c: {
+      key: 'c',
+      type: 'blockquote',
+      text: 'Charlie',
+      characterList: [
+        {
+          style: ['ITALIC'],
+          entity: null,
+        },
+        {
+          style: ['ITALIC'],
+          entity: null,
+        },
+        {
+          style: ['ITALIC'],
+          entity: null,
+        },
+        {
+          style: ['ITALIC'],
+          entity: null,
+        },
+        {
+          style: ['ITALIC'],
+          entity: null,
+        },
+        {
+          style: ['ITALIC'],
+          entity: null,
+        },
+        {
+          style: ['ITALIC'],
+          entity: null,
+        },
+      ],
+      depth: 0,
+      data: {},
+    },
+  });
 });
 
 test('convert from raw tree draft content state', () => {
@@ -121,5 +305,273 @@ test('convert from raw tree draft content state', () => {
     entityMap: {},
   };
 
-  expect(convertFromRawToDraftState(rawState).getBlockMap()).toMatchSnapshot();
+  assertDraftState(rawState, {
+    A: {
+      parent: null,
+      characterList: [],
+      data: {},
+      depth: 0,
+      key: 'A',
+      text: '',
+      type: 'unstyled',
+      children: ['B', 'E'],
+      prevSibling: null,
+      nextSibling: null,
+    },
+    B: {
+      parent: 'A',
+      characterList: [],
+      data: {},
+      depth: 0,
+      key: 'B',
+      text: '',
+      type: 'unstyled',
+      children: ['C', 'D'],
+      prevSibling: null,
+      nextSibling: 'E',
+    },
+    C: {
+      parent: 'B',
+      characterList: [
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+      ],
+      data: {},
+      depth: 0,
+      key: 'C',
+      text: 'left block',
+      type: 'unstyled',
+      children: [],
+      prevSibling: null,
+      nextSibling: 'D',
+    },
+    D: {
+      parent: 'B',
+      characterList: [
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+      ],
+      data: {},
+      depth: 0,
+      key: 'D',
+      text: 'right block',
+      type: 'unstyled',
+      children: [],
+      prevSibling: 'C',
+      nextSibling: null,
+    },
+    E: {
+      parent: 'A',
+      characterList: [
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+        {
+          style: [],
+          entity: null,
+        },
+      ],
+      data: {},
+      depth: 0,
+      key: 'E',
+      text: 'This is a tree based document!',
+      type: 'header-one',
+      children: [],
+      prevSibling: 'B',
+      nextSibling: null,
+    },
+  });
 });
