19fb3e77898232ca46c9b15ed8dd16aecfaa6a3a
mitermayer <mitermayer.reis@gmail.com>
2017-11-19 23:58:17 -0800

    Exploration - Feature tree / Convert from html tree data support
    Summary:
    This PR is part of a series of PR's that will be exploring **tree data block support** in Draft.
    
    **Convert from html tree data support**
    
    This PR adds support for converting from HTML to tree data, as a follow up we will then hook the paste processor with this too
    
    ***
    
    **Note:**
    This is unstable and not part of the public API and should not be used by
    production systems.
    Closes https://github.com/facebook/draft-js/pull/1518
    
    Differential Revision: D6372235
    
    fbshipit-source-id: fdfe81efcb81e31aa4bb61f5cfe7b21bf11317a4

--- src/model/encoding/__tests__/convertFromHTMLToContentBlocks-test.js
+++ src/model/encoding/__tests__/convertFromHTMLToContentBlocks-test.js
@@ -16,32 +16,22 @@ jest.disableAutomock();
 
 jest.mock('generateRandomKey');
 
-const convertFromHTMLToContentBlocks = require('convertFromHTMLToContentBlocks');
+const DefaultDraftBlockRenderMap = require('DefaultDraftBlockRenderMap');
 
-const assertConvertFromHTMLToContentBlocks = html_string => {
-  expect(
-    convertFromHTMLToContentBlocks(html_string).contentBlocks.map(block =>
-      block.toJS(),
-    ),
-  ).toMatchSnapshot();
+const convertFromHTMLToContentBlocks = require('convertFromHTMLToContentBlocks');
+const getSafeBodyFromHTML = require('getSafeBodyFromHTML');
+
+const DEFAULT_CONFIG = {
+  DOMBuilder: getSafeBodyFromHTML,
+  blockRenderMap: DefaultDraftBlockRenderMap,
+  experimentalTreeDataSupport: false,
 };
 
 const IMAGE_DATA_URL =
   'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///' +
   'yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
-const testConvertingAdjacentHtmlElementsToContentBlocks = (tag: string) => {
-  it(`must not merge tags when converting adjacent <${tag} />`, () => {
-    const html_string = `
-      <${tag}>a</${tag}>
-      <${tag}>b</${tag}>
-    `;
-
-    assertConvertFromHTMLToContentBlocks(html_string);
-  });
-};
-
-[
+const SUPPORTED_TAGS = [
   'blockquote',
   'div',
   'figure',
@@ -54,7 +44,81 @@ const testConvertingAdjacentHtmlElementsToContentBlocks = (tag: string) => {
   'li',
   'p',
   'pre',
-].forEach(tag => testConvertingAdjacentHtmlElementsToContentBlocks(tag));
+];
+
+const assertConvertFromHTMLToContentBlocks = (html_string, config = {}) => {
+  const options = {
+    ...DEFAULT_CONFIG,
+    ...config,
+  };
+
+  const {DOMBuilder, blockRenderMap, experimentalTreeDataSupport} = options;
+
+  expect(
+    convertFromHTMLToContentBlocks(
+      html_string,
+      DOMBuilder,
+      blockRenderMap,
+      experimentalTreeDataSupport,
+    ).contentBlocks.map(block => block.toJS()),
+  ).toMatchSnapshot();
+};
+
+const testConvertingAdjacentHtmlElementsToContentBlocks = (
+  tag: string,
+  experimentalTreeDataSupport?: boolean = false,
+) => {
+  test(`must not merge tags when converting adjacent <${tag} />`, () => {
+    const html_string = `
+      <${tag}>a</${tag}>
+      <${tag}>b</${tag}>
+    `;
+
+    assertConvertFromHTMLToContentBlocks(
+      html_string,
+      experimentalTreeDataSupport,
+    );
+  });
+};
+
+const normalizeBlock = block => {
+  const {type, depth, textBlock, characterList} = block;
+
+  return {
+    type,
+    depth,
+    textBlock,
+    characterList,
+  };
+};
+
+const testConvertingHtmlElementsToContentBlocksAndRootContentBlockNodesMatch = (
+  tag: string,
+) => {
+  test(`must convert root ContentBlockNodes to matching ContentBlock nodes for <${tag} />`, () => {
+    const {DOMBuilder, blockRenderMap} = DEFAULT_CONFIG;
+    const html_string = `<${tag}>a</${tag}> `;
+    expect(
+      convertFromHTMLToContentBlocks(
+        html_string,
+        DOMBuilder,
+        blockRenderMap,
+        false,
+      ).contentBlocks.map(block => normalizeBlock(block.toJS())),
+    ).toEqual(
+      convertFromHTMLToContentBlocks(
+        html_string,
+        DOMBuilder,
+        blockRenderMap,
+        true,
+      ).contentBlocks.map(block => normalizeBlock(block.toJS())),
+    );
+  });
+};
+
+SUPPORTED_TAGS.forEach(tag =>
+  testConvertingAdjacentHtmlElementsToContentBlocks(tag),
+);
 
 test('img with http protocol should have camera emoji content', () => {
   const blocks = convertFromHTMLToContentBlocks(
@@ -69,3 +133,43 @@ test('img with data protocol should be correctly parsed', () => {
   );
   expect(blocks.contentBlocks[0].text).toMatchSnapshot();
 });
+
+test('converts nested html blocks when experimentalTreeDataSupport is enabled', () => {
+  const html_string = `
+    <blockquote>
+      <h1>Hello World!</h1>
+      <p>lorem ipsum</p>
+    </blockquote>
+  `;
+
+  assertConvertFromHTMLToContentBlocks(html_string, {
+    experimentalTreeDataSupport: true,
+  });
+});
+
+test('converts deeply nested html blocks when experimentalTreeDataSupport is enabled', () => {
+  const html_string = `
+    <ol>
+      <li>Some quote</li>
+      <li>
+        <blockquote>
+          <h1>Hello World!</h1>
+          <p>lorem ipsum</p>
+        </blockquote>
+      </li>
+    </ol>
+  `;
+
+  assertConvertFromHTMLToContentBlocks(html_string, {
+    experimentalTreeDataSupport: true,
+  });
+});
+
+SUPPORTED_TAGS.forEach(tag =>
+  testConvertingAdjacentHtmlElementsToContentBlocks(tag, true),
+);
+
+// assert that using tree blocks and root content block nodes are equivalent
+SUPPORTED_TAGS.forEach(tag =>
+  testConvertingHtmlElementsToContentBlocksAndRootContentBlockNodesMatch(tag),
+);
