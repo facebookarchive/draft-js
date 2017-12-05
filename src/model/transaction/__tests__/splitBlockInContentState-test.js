57ea131188e5b3a583203ada3d486ca7e13631ae
mitermayer <mitermayer.reis@gmail.com>
2017-12-04 22:01:35 -0800

    Exploration - Feature tree / Adding support for splitting transactions
    - This PR adds support for tree block nodes spliting transactions

--- src/model/transaction/__tests__/splitBlockInContentState-test.js
+++ src/model/transaction/__tests__/splitBlockInContentState-test.js
@@ -16,15 +16,71 @@ jest.disableAutomock();
 
 jest.mock('generateRandomKey');
 
+const BlockMapBuilder = require('BlockMapBuilder');
+const ContentBlockNode = require('ContentBlockNode');
+const Immutable = require('immutable');
+const SelectionState = require('SelectionState');
+
 const getSampleStateForTesting = require('getSampleStateForTesting');
 const splitBlockInContentState = require('splitBlockInContentState');
 
+const {List} = Immutable;
+
 const {contentState, selectionState} = getSampleStateForTesting();
 
-const assertSplitBlockInContentState = selection => {
+const contentBlockNodes = [
+  new ContentBlockNode({
+    key: 'A',
+    nextSibling: 'B',
+    text: 'Alpha',
+  }),
+  new ContentBlockNode({
+    key: 'B',
+    prevSibling: 'A',
+    nextSibling: 'G',
+    children: List(['C', 'F']),
+  }),
+  new ContentBlockNode({
+    parent: 'B',
+    key: 'C',
+    nextSibling: 'F',
+    children: List(['D', 'E']),
+  }),
+  new ContentBlockNode({
+    parent: 'C',
+    key: 'D',
+    nextSibling: 'E',
+    text: 'Delta',
+  }),
+  new ContentBlockNode({
+    parent: 'C',
+    key: 'E',
+    prevSibling: 'D',
+    text: 'Elephant',
+  }),
+  new ContentBlockNode({
+    parent: 'B',
+    key: 'F',
+    prevSibling: 'C',
+    text: 'Fire',
+  }),
+  new ContentBlockNode({
+    key: 'G',
+    prevSibling: 'B',
+    text: 'Gorila',
+  }),
+];
+const treeSelectionState = SelectionState.createEmpty('A');
+const treeContentState = contentState.set(
+  'blockMap',
+  BlockMapBuilder.createFromArray(contentBlockNodes),
+);
+
+const assertSplitBlockInContentState = (selection, content = contentState) => {
   expect(
-    splitBlockInContentState(contentState, selection)
+    splitBlockInContentState(content, selection)
       .getBlockMap()
+      .toIndexedSeq()
       .toJS(),
   ).toMatchSnapshot();
 };
@@ -66,5 +122,88 @@ test('must split at the end of a block', () => {
       anchorOffset: SPLIT_OFFSET,
       focusOffset: SPLIT_OFFSET,
     }),
+  );
+});
+
+test('must be restricted to collapsed selections for ContentBlocks', () => {
+  expect(() => {
+    const nonCollapsed = treeSelectionState.set('focusOffset', 1);
+    return splitBlockInContentState(treeContentState, nonCollapsed);
+  }).toThrow();
+
+  expect(() => {
+    return splitBlockInContentState(treeContentState, treeSelectionState);
+  }).not.toThrow();
+});
+
+test('must be restricted to ContentBlocks that do not have children', () => {
+  expect(() => {
+    const invalidSelection = treeSelectionState.merge({
+      anchorKey: 'B',
+      focusKey: 'B',
+    });
+    return splitBlockInContentState(treeContentState, invalidSelection);
+  }).toThrow();
+});
+
+test('must split at the beginning of a root ContentBlock', () => {
+  assertSplitBlockInContentState(treeSelectionState, treeContentState);
+});
+
+test('must split at the beginning of a nested ContentBlock', () => {
+  assertSplitBlockInContentState(
+    treeSelectionState.merge({
+      anchorKey: 'D',
+      focusKey: 'D',
+    }),
+    treeContentState,
+  );
+});
+
+test('must split within a root ContentBlock', () => {
+  const SPLIT_OFFSET = 3;
+  assertSplitBlockInContentState(
+    treeSelectionState.merge({
+      anchorOffset: SPLIT_OFFSET,
+      focusOffset: SPLIT_OFFSET,
+    }),
+    treeContentState,
+  );
+});
+
+test('must split within a nested ContentBlock', () => {
+  const SPLIT_OFFSET = 3;
+  assertSplitBlockInContentState(
+    treeSelectionState.merge({
+      anchorOffset: SPLIT_OFFSET,
+      focusOffset: SPLIT_OFFSET,
+      anchorKey: 'E',
+      focusKey: 'E',
+    }),
+    treeContentState,
+  );
+});
+
+test('must split at the end of a root ContentBlock', () => {
+  const SPLIT_OFFSET = contentBlockNodes[0].getLength();
+  assertSplitBlockInContentState(
+    treeSelectionState.merge({
+      anchorOffset: SPLIT_OFFSET,
+      focusOffset: SPLIT_OFFSET,
+    }),
+    treeContentState,
+  );
+});
+
+test('must split at the end of a nested ContentBlock', () => {
+  const SPLIT_OFFSET = contentBlockNodes[3].getLength();
+  assertSplitBlockInContentState(
+    treeSelectionState.merge({
+      anchorOffset: SPLIT_OFFSET,
+      focusOffset: SPLIT_OFFSET,
+      anchorKey: 'D',
+      focusKey: 'D',
+    }),
+    treeContentState,
   );
 });
