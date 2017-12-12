02668d6df38fdf24923fbb7f045b29922498881f
Florian Tatzky <florian.tatzky@bild.de>
2017-12-12 15:56:09 +0100

    fixed a bug related to draft losing its selection

--- src/component/selection/setDraftEditorSelection.js
+++ src/component/selection/setDraftEditorSelection.js
@@ -303,9 +303,14 @@ function addFocusToSelection(
     // Additionally, clone the selection range. IE11 throws an
     // InvalidStateError when attempting to access selection properties
     // after the range is detached.
-    var range = selection.getRangeAt(0);
-    range.setEnd(node, offset);
-    selection.addRange(range.cloneRange());
+    //
+    // Added rangeCount check
+    // https://stackoverflow.com/questions/22935320/uncaught-indexsizeerror-failed-to-execute-getrangeat-on-selection-0-is-not
+    if (selection.rangeCount > 0) {
+      var range = selection.getRangeAt(0);
+      range.setEnd(node, offset);
+      selection.addRange(range.cloneRange());
+    }
   }
 }
 
