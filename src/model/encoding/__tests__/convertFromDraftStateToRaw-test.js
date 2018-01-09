0059dd46f4d23af7d9803316aa93d8deddb5e8ae
Frank Thompson <frankthompson@fb.com>
2018-01-08 16:23:53 -0800

    make encodeRawBlocks support non-contiguous entities
    Summary: `encodeRawBlocks` was improperly incrementing `entityStorageKey` when processing non-contiguous entities. This caused all entities after the non-contiguous one to have incorrect keys.
    
    Reviewed By: sophiebits
    
    Differential Revision: D6667822
    
    fbshipit-source-id: 6222ea844893ab75aad8171224d01ad893ecf44f

--- src/model/encoding/__tests__/convertFromDraftStateToRaw-test.js
+++ src/model/encoding/__tests__/convertFromDraftStateToRaw-test.js
@@ -16,7 +16,11 @@
 jest.disableAutomock();
 
 const BlockMapBuilder = require('BlockMapBuilder');
+const CharacterMetadata = require('CharacterMetadata');
+const ContentBlock = require('ContentBlock');
 const ContentBlockNode = require('ContentBlockNode');
+const ContentState = require('ContentState');
+const DraftEntityInstance = require('DraftEntityInstance');
 const Immutable = require('immutable');
 
 const convertFromDraftStateToRaw = require('convertFromDraftStateToRaw');
@@ -59,6 +63,49 @@ const treeContentState = contentState.set(
   ]),
 );
 
+const getMetadata = entityKey =>
+  Immutable.Repeat(CharacterMetadata.create({entity: entityKey}), 5);
+const getLink = entityKey =>
+  new DraftEntityInstance({
+    type: 'LINK',
+    mutabiltity: 'MUTABLE',
+    data: {
+      url: `www.${entityKey}.com`,
+    },
+  });
+// We start numbering our entities with '2' because getSampleStateForTesting
+// already created an entity with key '1'.
+const contentStateWithNonContiguousEntities = ContentState.createFromBlockArray(
+  [
+    new ContentBlock({
+      key: 'a',
+      type: 'unstyled',
+      text: 'link2 link2 link3',
+      characterList: getMetadata('2')
+        .toList()
+        .push(CharacterMetadata.EMPTY)
+        .concat(getMetadata('2'))
+        .push(CharacterMetadata.EMPTY)
+        .concat(getMetadata('3')),
+    }),
+    new ContentBlock({
+      key: 'b',
+      type: 'unstyled',
+      text: 'link4 link2 link5',
+      characterList: getMetadata('4')
+        .toList()
+        .push(CharacterMetadata.EMPTY)
+        .concat(getMetadata('2'))
+        .push(CharacterMetadata.EMPTY)
+        .concat(getMetadata('5')),
+    }),
+  ],
+)
+  .addEntity(getLink('2'))
+  .addEntity(getLink('3'))
+  .addEntity(getLink('4'))
+  .addEntity(getLink('5'));
+
 const assertConvertFromDraftStateToRaw = content => {
   expect(convertFromDraftStateToRaw(content)).toMatchSnapshot();
 };
@@ -69,4 +116,8 @@ test('must be able to convert from draft state with ContentBlock to raw', () => 
 
 test('must be able to convert from draft state with ContentBlockNode to raw', () => {
   assertConvertFromDraftStateToRaw(treeContentState);
+});
+
+test('must be able to convert from draft state with noncontiguous entities to raw', () => {
+  assertConvertFromDraftStateToRaw(contentStateWithNonContiguousEntities);
 });
