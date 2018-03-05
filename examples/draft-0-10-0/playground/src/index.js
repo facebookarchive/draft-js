fb9ba7988bdd81b52b7336eedd6b561b467409db
mitermayer <mitermayer.reis@gmail.com>
2018-03-05 09:05:23 -0800

    Loging the overwrites applyed to the playground

--- examples/draft-0-10-0/playground/src/index.js
+++ examples/draft-0-10-0/playground/src/index.js
@@ -5,5 +5,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log("Applying feature flag overwrites: ", GkManager);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
