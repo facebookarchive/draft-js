857876791c50d712115644fa661c8bf5a08bba37
Sannikov <yury.sannikov@gmail.com>
2018-07-25 13:09:45 -0400

    fix lint error

--- src/model/encoding/__tests__/convertFromHTMLToContentBlocks-test.js
+++ src/model/encoding/__tests__/convertFromHTMLToContentBlocks-test.js
@@ -321,7 +321,7 @@ test('Should preserve spacing around inline tags', () => {
 test('Should convert heading block after converting new line string', () => {
   convertFromHTML('a\n');
   const html_string = `<h1>heading</h1>`;
-  const { contentBlocks } = convertFromHTML(html_string);
+  const {contentBlocks} = convertFromHTML(html_string);
   expect(contentBlocks.length).toBe(1);
   expect(contentBlocks[0].getType()).toBe('header-one');
   expect(contentBlocks[0].getText()).toBe('heading');
