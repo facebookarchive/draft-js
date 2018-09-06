022798c34c138f8e5c025d28fdf0797776d61555
Nivedita Chopra <niveditc@fb.com>
2018-09-06 09:58:51 -0700

    var => const on remaining file
    Summary: Saw this flagged in Quality Analyser tool. Also added flow to the file.
    
    Reviewed By: vdurmont
    
    Differential Revision: D9674403
    
    fbshipit-source-id: 1705cb7271b0eeda397d79a7b5f853f28fe94de1

--- src/model/encoding/__tests__/decodeEntityRanges-test.js
+++ src/model/encoding/__tests__/decodeEntityRanges-test.js
@@ -7,6 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  *
  * @emails oncall+draft_js
+ * @flow strict-local
  * @format
  */
 
@@ -14,15 +15,15 @@
 
 jest.disableAutomock();
 
-var decodeEntityRanges = require('decodeEntityRanges');
+const decodeEntityRanges = require('decodeEntityRanges');
 
 test('must decode when no entities present', () => {
-  var decoded = decodeEntityRanges(' '.repeat(20), []);
+  const decoded = decodeEntityRanges(' '.repeat(20), []);
   expect(decoded).toMatchSnapshot();
 });
 
 test('must decode when an entity is present', () => {
-  var decoded = decodeEntityRanges(' '.repeat(5), [
+  const decoded = decodeEntityRanges(' '.repeat(5), [
     {
       offset: 2,
       length: 2,
@@ -33,7 +34,7 @@ test('must decode when an entity is present', () => {
 });
 
 test('must decode when multiple entities present', () => {
-  var decoded = decodeEntityRanges(' '.repeat(8), [
+  const decoded = decodeEntityRanges(' '.repeat(8), [
     {
       offset: 2,
       length: 2,
@@ -49,7 +50,7 @@ test('must decode when multiple entities present', () => {
 });
 
 test('must decode when an entity is present more than once', () => {
-  var decoded = decodeEntityRanges(' '.repeat(8), [
+  const decoded = decodeEntityRanges(' '.repeat(8), [
     {
       offset: 2,
       length: 2,
@@ -65,7 +66,7 @@ test('must decode when an entity is present more than once', () => {
 });
 
 test('must handle ranges that include surrogate pairs', () => {
-  var decoded = decodeEntityRanges('Take a \uD83D\uDCF7 #selfie', [
+  const decoded = decodeEntityRanges('Take a \uD83D\uDCF7 #selfie', [
     {
       offset: 6,
       length: 5,
