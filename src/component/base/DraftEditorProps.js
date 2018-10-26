59dd225738167d213558934a6e45c7f0a1c5e0c1
Avik Chaudhuri <avik@fb.com>
2018-10-25 22:34:31 -0700

    flow 0.84 www deploy
    Summary:
    The controller you requested could not be found.
    The controller you requested could not be found.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D10851590
    
    fbshipit-source-id: 7858075ed2dd740f5105aa668c42c2e6d96aa0ef

--- src/component/base/DraftEditorProps.js
+++ src/component/base/DraftEditorProps.js
@@ -47,7 +47,6 @@ export type DraftEditorProps = {
   // styling and formatting when re-applying styles.
   editorKey?: string,
 
-  // $FlowFixMe in practice people pass other renderable things here
   placeholder?: string,
 
   // Specify whether text alignment should be forced in a direction
