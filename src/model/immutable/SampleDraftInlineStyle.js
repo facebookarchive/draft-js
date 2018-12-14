8dd6dda034d06326e77044677327734e02c585fd
Gabe Levi <gabe@fb.com>
2018-12-14 14:09:01 -0800

    Remove unused suppression comments from www as of v0.89
    Summary:
    This makes a best effort attempt to clean up unused suppressions in www.
    
    * Removed comments from www
    
    ```
    $ ~/flow/tool remove-comments --bin `which flow` --check status .
    Removed 565 comments in 360 files
    Ignored 38 comments in 18 files due to -flowtest.js suffix or __flowtests__ directory. Run with `--include-flowtest` to also remove those files.
    ```
    
    * Revert generated code (61 files P60440612)
    
    * Manually fixed a few things in www and mobile. Mostly flowlint comments or poorly scoped comments
    
    * Ran prettier
    
    * Removed comments from mobile
    
    ```
    [glevi@devbig023 www-hg]$ ~/flow/tool remove-comments --bin `which flow` --check status html/js/mobile
    Removed 11 comments in 11 files
    Ignored 6 comments in 3 files due to -flowtest.js suffix or __flowtests__ directory. Run with `--include-flowtest` to also remove those files.
    ```
    
    * Reverted generated files
    
    * Manually fixed mobile again (a few malformed flowlint comments)
    
    drop-conflicts
    bypass-lint
    
    Reviewed By: jbrown215
    
    Differential Revision: D13450730
    
    fbshipit-source-id: 6fdbd00e36e439388e221fc233f2ecf01fe07442

--- src/model/immutable/SampleDraftInlineStyle.js
+++ src/model/immutable/SampleDraftInlineStyle.js
@@ -16,50 +16,20 @@
 const {OrderedSet} = require('immutable');
 
 module.exports = {
-  /* $FlowFixMe(>=0.85.0 site=www,mobile) This comment suppresses an error
-   * found when Flow v0.85 was deployed. To see the error, delete this comment
-   * and run Flow. */
   BOLD: (OrderedSet.of('BOLD'): OrderedSet<$FlowFixMe>),
-  /* $FlowFixMe(>=0.85.0 site=www,mobile) This comment suppresses an error
-   * found when Flow v0.85 was deployed. To see the error, delete this comment
-   * and run Flow. */
   BOLD_ITALIC: (OrderedSet.of('BOLD', 'ITALIC'): OrderedSet<$FlowFixMe>),
-  /* $FlowFixMe(>=0.85.0 site=www,mobile) This comment suppresses an error
-   * found when Flow v0.85 was deployed. To see the error, delete this comment
-   * and run Flow. */
   BOLD_ITALIC_UNDERLINE: (OrderedSet.of(
     'BOLD',
     'ITALIC',
     'UNDERLINE',
   ): OrderedSet<$FlowFixMe>),
-  /* $FlowFixMe(>=0.85.0 site=www,mobile) This comment suppresses an error
-   * found when Flow v0.85 was deployed. To see the error, delete this comment
-   * and run Flow. */
   BOLD_UNDERLINE: (OrderedSet.of('BOLD', 'UNDERLINE'): OrderedSet<$FlowFixMe>),
-  /* $FlowFixMe(>=0.85.0 site=www,mobile) This comment suppresses an error
-   * found when Flow v0.85 was deployed. To see the error, delete this comment
-   * and run Flow. */
   CODE: (OrderedSet.of('CODE'): OrderedSet<$FlowFixMe>),
-  /* $FlowFixMe(>=0.85.0 site=www,mobile) This comment suppresses an error
-   * found when Flow v0.85 was deployed. To see the error, delete this comment
-   * and run Flow. */
   ITALIC: (OrderedSet.of('ITALIC'): OrderedSet<$FlowFixMe>),
-  /* $FlowFixMe(>=0.85.0 site=www,mobile) This comment suppresses an error
-   * found when Flow v0.85 was deployed. To see the error, delete this comment
-   * and run Flow. */
   ITALIC_UNDERLINE: (OrderedSet.of('ITALIC', 'UNDERLINE'): OrderedSet<
     $FlowFixMe,
   >),
-  /* $FlowFixMe(>=0.85.0 site=www,mobile) This comment suppresses an error
-   * found when Flow v0.85 was deployed. To see the error, delete this comment
-   * and run Flow. */
   NONE: (OrderedSet(): OrderedSet<$FlowFixMe>),
-  /* $FlowFixMe(>=0.85.0 site=www,mobile) This comment suppresses an error
-   * found when Flow v0.85 was deployed. To see the error, delete this comment
-   * and run Flow. */
   STRIKETHROUGH: (OrderedSet.of('STRIKETHROUGH'): OrderedSet<$FlowFixMe>),
-  /* $FlowFixMe(>=0.85.0 site=www,mobile) This comment suppresses an error
-   * found when Flow v0.85 was deployed. To see the error, delete this comment
-   * and run Flow. */
   UNDERLINE: (OrderedSet.of('UNDERLINE'): OrderedSet<$FlowFixMe>),
 };
