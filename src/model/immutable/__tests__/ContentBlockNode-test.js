5e6901ac6ff38649ca5d2186833e88a1aafb41bc
Claudio Procida <procidac@fb.com>
2019-01-03 11:53:25 +0100

    Fixes misspelling in copyright header

--- src/model/immutable/__tests__/ContentBlockNode-test.js
+++ src/model/immutable/__tests__/ContentBlockNode-test.js
@@ -2,7 +2,7 @@
  * Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An addtestional grant
+ * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
  * @emails oncall+draft_js
