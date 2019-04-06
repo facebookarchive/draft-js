efc142360c49a8013b0a4cabdda1b19d34d3c227
Suraj Keshri <surajkeshri@gmail.com>
2019-04-06 14:18:07 -0400

    Fixed the cursor moving to top of editor when text is cut from a code block

--- src/component/handlers/edit/editOnCut.js
+++ src/component/handlers/edit/editOnCut.js
@@ -18,6 +18,7 @@ import type DraftEditor from 'DraftEditor.react';
 const DraftModifier = require('DraftModifier');
 const EditorState = require('EditorState');
 const Style = require('Style');
+const ReactDOM = require('ReactDOM');
 
 const getFragmentFromSelection = require('getFragmentFromSelection');
 const getScrollPosition = require('getScrollPosition');
@@ -34,8 +35,8 @@ const getScrollPosition = require('getScrollPosition');
 function editOnCut(editor: DraftEditor, e: SyntheticClipboardEvent<>): void {
   const editorState = editor._latestEditorState;
   const selection = editorState.getSelection();
-  const element = e.target;
-  let scrollPosition;
+  // const element = e.target;
+  // let scrollPosition;
 
   // No selection, so there's nothing to cut.
   if (selection.isCollapsed()) {
@@ -43,11 +44,16 @@ function editOnCut(editor: DraftEditor, e: SyntheticClipboardEvent<>): void {
     return;
   }
 
+  // CHANGES COPIED FROM https://github.com/facebook/draft-js/pull/1428/files/45b30b4d30b46691b9c5976885265f86e3b524a2
+
   // Track the current scroll position so that it can be forced back in place
   // after the editor regains control of the DOM.
-  if (element instanceof Node) {
-    scrollPosition = getScrollPosition(Style.getScrollParent(element));
-  }
+  // if (element instanceof Node) {
+  //   scrollPosition = getScrollPosition(Style.getScrollParent(element));
+  // }
+  const editorNode = ReactDOM.findDOMNode(editor.editor);
+  const scrollParent = Style.getScrollParent(editorNode);
+  const scrollPosition = getScrollPosition(scrollParent);
 
   const fragment = getFragmentFromSelection(editorState);
   editor.setClipboard(fragment);
