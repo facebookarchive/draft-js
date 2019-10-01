33eeef6a8d24f1208a511fde16f896a31eb2d693
Richard Juchelka <richardj@kentico.com>
2019-10-01 17:27:06 +0200

    KCL-2962 - Hardcode LTR direction
    Increase project version

--- src/model/immutable/EditorBidiService.js
+++ src/model/immutable/EditorBidiService.js
@@ -13,37 +13,25 @@
 
 import type ContentState from 'ContentState';
 
-const UnicodeBidiService = require('UnicodeBidiService');
-
 const Immutable = require('immutable');
-const nullthrows = require('nullthrows');
+const UnicodeBidiDirection = require('UnicodeBidiDirection');
 
 const {OrderedMap} = Immutable;
-
-let bidiService;
 
 const EditorBidiService = {
   getDirectionMap: function(
     content: ContentState,
     prevBidiMap: ?OrderedMap<any, any>,
   ): OrderedMap<any, any> {
-    if (!bidiService) {
-      bidiService = new UnicodeBidiService();
-    } else {
-      bidiService.reset();
-    }
-
     const blockMap = content.getBlockMap();
-    const nextBidi = blockMap
-      .valueSeq()
-      .map(block => nullthrows(bidiService).getDirection(block.getText()));
-    const bidiMap = OrderedMap(blockMap.keySeq().zip(nextBidi));
 
-    if (prevBidiMap != null && Immutable.is(prevBidiMap, bidiMap)) {
-      return prevBidiMap;
+    if (prevBidiMap && blockMap.size === prevBidiMap.size) {
+      if (blockMap.keySeq().every(blockKey => prevBidiMap.has(blockKey))) {
+        return prevBidiMap;
+      }
     }
 
-    return bidiMap;
+    return blockMap.map(() => UnicodeBidiDirection.LTR).toOrderedMap();
   },
 };
 
