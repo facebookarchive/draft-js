af6757555640f5cff2d3d0290afcbc8f3e9b023a
Panagiotis Vekris <pvekris@fb.com>
2020-04-20 19:57:03 -0700

    Flow v0.123.0 in www
    Summary:
    ```
    cd ~/fbsource/fbcode
    flow/facebook/deploy_www.sh 0.123.0
    ```
    
    Reviewed By: dsainati1
    
    Differential Revision: D21072854
    
    fbshipit-source-id: 862c05a900475320eae125440f7cb199e92152e5

--- src/model/encoding/__tests__/convertFromHTMLToContentBlocks-test.js
+++ src/model/encoding/__tests__/convertFromHTMLToContentBlocks-test.js
@@ -81,9 +81,6 @@ const convertFromHTML = (html_string, config) => {
    * Flow. */
   const options = {
     ...DEFAULT_CONFIG,
-    /* $FlowFixMe(>=0.111.0) This comment suppresses an error found when Flow
-     * v0.111.0 was deployed. To see the error, delete this comment and run
-     * Flow. */
     ...config,
   };
 
