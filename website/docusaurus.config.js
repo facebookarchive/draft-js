4c376678f7b68e8bbc1452bf2356ce5873a20c0f
John Knox <jknox@fb.com>
2020-10-15 04:09:17 -0700

    Move draft-js wiki to the internal website
    Summary:
    Putting out this diff to see if it looks useful. The script has taken the draft js wiki and just aded it into an FB Internal section of the website. This means for authoring updates, and for reading documentation, you only need to go to the one docusaurus site.
    
    This is an automated change produced by running:
    ```
    arc scarf static-docs-from-wiki --wiki_name Draft-js --site_dir scripts/draft-js/__github__/website --usage_warning Y
    ```
    
    Reviewed By: mrkev
    
    Differential Revision: D23905079
    
    fbshipit-source-id: 4fc6f26ede7acf2f98bfbc1982f10387f9879be4

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,10 +1,13 @@
+const {
+  fbInternalOnly
+} = require('internaldocs-fb-helpers');
+
 /**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 module.exports = {
   title: 'Draft.js',
   tagline: 'Rich Text Editor Framework for React',
@@ -13,92 +16,81 @@ module.exports = {
   organizationName: 'facebook',
   projectName: 'draft-js',
   favicon: 'img/draftjs-logo.ico',
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          path: '../docs',
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/facebook/draft-js/edit/master/docs',
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
+  presets: [['@docusaurus/preset-classic', {
+    docs: {
+      path: '../docs',
+      sidebarPath: require.resolve('./sidebars.js'),
+      editUrl: 'https://github.com/facebook/draft-js/edit/master/docs',
+      showLastUpdateAuthor: true,
+      showLastUpdateTime: true
+    },
+    theme: {
+      customCss: require.resolve('./src/css/custom.css')
+    }
+  }]],
   themeConfig: {
     disableDarkMode: true,
     navbar: {
       title: 'Draft.js',
       logo: {
         alt: 'Draft.js Logo',
-        src: 'img/draftjs-logo.svg',
+        src: 'img/draftjs-logo.svg'
       },
-      links: [
-        {to: 'docs/getting-started', label: 'Docs', position: 'right'},
-        {
-          href: 'https://github.com/facebook/draft-js',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
+      links: [{
+        to: 'docs/getting-started',
+        label: 'Docs',
+        position: 'right'
+      }, {
+        href: 'https://github.com/facebook/draft-js',
+        label: 'GitHub',
+        position: 'right'
+      }, ...fbInternalOnly([{
+        "to": "docs/fb/index",
+        "label": "FB Internal",
+        "position": "right"
+      }])]
     },
     algolia: {
       apiKey: 'ae94c9e3ee00ea8edddd484adafc37cd',
-      indexName: 'draft-js',
+      indexName: 'draft-js'
     },
     googleAnalytics: {
-      trackingID: 'UA-44373548-19',
+      trackingID: 'UA-44373548-19'
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Getting Started',
-              to: 'docs/getting-started',
-            },
-            {
-              label: 'API Reference',
-              to: 'docs/api-reference-editor',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/draftjs',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/draft_js',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/draft-js',
-            },
-          ],
-        },
-      ],
+      links: [{
+        title: 'Docs',
+        items: [{
+          label: 'Getting Started',
+          to: 'docs/getting-started'
+        }, {
+          label: 'API Reference',
+          to: 'docs/api-reference-editor'
+        }]
+      }, {
+        title: 'Community',
+        items: [{
+          label: 'Stack Overflow',
+          href: 'https://stackoverflow.com/questions/tagged/draftjs'
+        }, {
+          label: 'Twitter',
+          href: 'https://twitter.com/draft_js'
+        }]
+      }, {
+        title: 'More',
+        items: [{
+          label: 'GitHub',
+          href: 'https://github.com/facebook/draft-js'
+        }]
+      }],
       logo: {
         alt: 'Facebook Open Source Logo',
         src: '/img/oss_logo.png',
-        href: 'https://opensource.facebook.com/',
+        href: 'https://opensource.facebook.com/'
       },
-      copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc.`,
-    },
+      copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc.`
+    }
   },
+  plugins: [require.resolve('docusaurus-plugin-internaldocs-fb')],
 };
