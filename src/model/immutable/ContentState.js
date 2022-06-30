70414e8186cff1ac8da3c037e291997cde331f39
Brad Zacher <bradzacher@fb.com>
2022-06-29 17:13:56 -0700

    remove (this: any) casts from the codebase
    Summary:
    it's silly to hide errors by just casting to `any` and forgetting about it.
    replace them with suppressions.
    
    reapplication of D37478365 (https://github.com/facebook/draft-js/commit/8417079ac7c005e1f52d889885c2eb7ad4c1d9e5) without the broken code.
    
    Reviewed By: pieterv
    
    Differential Revision: D37530690
    
    fbshipit-source-id: acdb8174b81b0d28550f92996bc84059ed132759

--- src/model/immutable/ContentState.js
+++ src/model/immutable/ContentState.js
@@ -79,15 +79,18 @@ class ContentState extends ContentStateRecord {
   }
 
   getBlockMap(): BlockMap {
-    return (this: any).get('blockMap');
+    // $FlowFixMe[prop-missing] found when removing casts of this to any
+    return this.get('blockMap');
   }
 
   getSelectionBefore(): SelectionState {
-    return (this: any).get('selectionBefore');
+    // $FlowFixMe[prop-missing] found when removing casts of this to any
+    return this.get('selectionBefore');
   }
 
   getSelectionAfter(): SelectionState {
-    return (this: any).get('selectionAfter');
+    // $FlowFixMe[prop-missing] found when removing casts of this to any
+    return this.get('selectionAfter');
   }
 
   getBlockForKey(key: string): BlockNodeRecord {
@@ -227,15 +230,18 @@ class ContentState extends ContentStateRecord {
   }
 
   setSelectionBefore(selection: SelectionState): ContentState {
-    return (this: any).set('selectionBefore', selection);
+    // $FlowFixMe[prop-missing] found when removing casts of this to any
+    return this.set('selectionBefore', selection);
   }
 
   setSelectionAfter(selection: SelectionState): ContentState {
-    return (this: any).set('selectionAfter', selection);
+    // $FlowFixMe[prop-missing] found when removing casts of this to any
+    return this.set('selectionAfter', selection);
   }
 
   setBlockMap(blockMap: BlockMap): ContentState {
-    return (this: any).set('blockMap', blockMap);
+    // $FlowFixMe[prop-missing] found when removing casts of this to any
+    return this.set('blockMap', blockMap);
   }
 
   static createFromBlockArray(
